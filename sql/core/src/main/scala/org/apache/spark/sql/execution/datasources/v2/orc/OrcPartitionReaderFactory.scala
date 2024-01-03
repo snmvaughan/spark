841615634beb4c24c2a90d9af08f23d7cc6c9bf0
sychen <sychen@ctrip.com>
2024-01-03 14:29:00 -0800

    [SPARK-44556][SQL] Reuse `OrcTail` when enable vectorizedReader
    ### What changes were proposed in this pull request?
    Reuse `OrcTail`
    
    ### Why are the changes needed?
    
    Avoid constructing `OrcTail` repeatedly when reading the same ORC file.
    
    The ORC reader updates the `OrcTail` to the `ReaderOptions`.
    
    org.apache.orc.impl.ReaderImpl#ReaderImpl
    ```java
          OrcTail orcTail = options.getOrcTail();
          if (orcTail == null) {
            tail = extractFileTail(getFileSystem(), path, options.getMaxLength());
            options.orcTail(tail);
          } else {
            checkOrcVersion(path, orcTail.getPostScript());
            tail = orcTail;
          }
    ```
    https://github.com/apache/orc/blob/3b448e004bd22e0cb8f71e18077f142a6535bc45/java/core/src/java/org/apache/orc/impl/ReaderImpl.java#L567-L574
    
    In addition, it can reduce one reading.
    
    org.apache.orc.impl.ReaderImpl#extractFileTail
    ```java
        file = fs.open(path);
    ```
    
    https://github.com/apache/orc/blob/3b448e004bd22e0cb8f71e18077f142a6535bc45/java/core/src/java/org/apache/orc/impl/ReaderImpl.java#L775-L781
    
    ### Does this PR introduce _any_ user-facing change?
    No
    
    ### How was this patch tested?
    exist UT
    
    Closes #42168 from cxzl25/SPARK-44556.
    
    Authored-by: sychen <sychen@ctrip.com>
    Signed-off-by: Dongjoon Hyun <dhyun@apple.com>
    (cherry picked from commit 5c10fb3e509ab64f7d67d37ae6dbcfb3f6b02efc)
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/orc/OrcPartitionReaderFactory.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/orc/OrcPartitionReaderFactory.scala
@@ -86,7 +86,7 @@ case class OrcPartitionReaderFactory(
     }
     val filePath = file.toPath
 
-    val orcSchema = Utils.tryWithResource(createORCReader(filePath, conf))(_.getSchema)
+    val orcSchema = Utils.tryWithResource(createORCReader(filePath, conf)._1)(_.getSchema)
     val resultedColPruneInfo = OrcUtils.requestedColumnIds(
       isCaseSensitive, dataSchema, readDataSchema, orcSchema, conf)
 
@@ -126,8 +126,8 @@ case class OrcPartitionReaderFactory(
       return buildColumnarReaderWithAggregates(file, conf)
     }
     val filePath = file.toPath
-
-    val orcSchema = Utils.tryWithResource(createORCReader(filePath, conf))(_.getSchema)
+    lazy val (reader, readerOptions) = createORCReader(filePath, conf)
+    val orcSchema = Utils.tryWithResource(reader)(_.getSchema)
     val resultedColPruneInfo = OrcUtils.requestedColumnIds(
       isCaseSensitive, dataSchema, readDataSchema, orcSchema, conf)
 
@@ -147,7 +147,7 @@ case class OrcPartitionReaderFactory(
       val taskAttemptContext = new TaskAttemptContextImpl(taskConf, attemptId)
 
       val batchReader = new OrcColumnarBatchReader(capacity)
-      batchReader.initialize(fileSplit, taskAttemptContext)
+      batchReader.initialize(fileSplit, taskAttemptContext, readerOptions.getOrcTail)
       val requestedPartitionColIds =
         Array.fill(readDataSchema.length)(-1) ++ Range(0, partitionSchema.length)
 
@@ -161,7 +161,9 @@ case class OrcPartitionReaderFactory(
     }
   }
 
-  private def createORCReader(filePath: Path, conf: Configuration): Reader = {
+  private def createORCReader(
+      filePath: Path,
+      conf: Configuration): (Reader, OrcFile.ReaderOptions) = {
     OrcConf.IS_SCHEMA_EVOLUTION_CASE_SENSITIVE.setBoolean(conf, isCaseSensitive)
 
     val fs = filePath.getFileSystem(conf)
@@ -170,7 +172,7 @@ case class OrcPartitionReaderFactory(
 
     pushDownPredicates(reader.getSchema, conf)
 
-    reader
+    (reader, readerOptions)
   }
 
   /**
@@ -183,7 +185,7 @@ case class OrcPartitionReaderFactory(
     new PartitionReader[InternalRow] {
       private var hasNext = true
       private lazy val row: InternalRow = {
-        Utils.tryWithResource(createORCReader(filePath, conf)) { reader =>
+        Utils.tryWithResource(createORCReader(filePath, conf)._1) { reader =>
           OrcUtils.createAggInternalRowFromFooter(
             reader, filePath.toString, dataSchema, partitionSchema, aggregation.get,
             readDataSchema, file.partitionValues)
@@ -211,7 +213,7 @@ case class OrcPartitionReaderFactory(
     new PartitionReader[ColumnarBatch] {
       private var hasNext = true
       private lazy val batch: ColumnarBatch = {
-        Utils.tryWithResource(createORCReader(filePath, conf)) { reader =>
+        Utils.tryWithResource(createORCReader(filePath, conf)._1) { reader =>
           val row = OrcUtils.createAggInternalRowFromFooter(
             reader, filePath.toString, dataSchema, partitionSchema, aggregation.get,
             readDataSchema, file.partitionValues)
