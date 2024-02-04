44b997beec05b5ca07755fc3e96d87de3c7d4caf
Dongjoon Hyun <dhyun@apple.com>
2024-02-04 14:44:51 +0800

    [SPARK-46899][CORE][FOLLOWUP] Enable `/workers/kill` if `spark.decommission.enabled=true`
    This PR aims to re-enable `/workers/kill` API if `spark.decommission.enabled=true` as a follow-up of
    - #44926
    
    To address this review comment in order to prevent a regression.
    - https://github.com/apache/spark/pull/44926#pullrequestreview-1854788375
    
    No, this will recover the previous feature.
    
    Manual review.
    
    No.
    
    Closes #45010 from dongjoon-hyun/SPARK-46899-2.
    
    Authored-by: Dongjoon Hyun <dhyun@apple.com>
    Signed-off-by: Kent Yao <yao@apache.org>

--- core/src/main/scala/org/apache/spark/deploy/master/ui/MasterWebUI.scala
+++ core/src/main/scala/org/apache/spark/deploy/master/ui/MasterWebUI.scala
@@ -42,7 +42,7 @@ class MasterWebUI(
 
   val masterEndpointRef = master.self
   val killEnabled = master.conf.get(UI_KILL_ENABLED)
-  val decommissionDisabled = !master.conf.get(DECOMMISSION_ENABLED)
+  val decommissionEnabled = master.conf.get(DECOMMISSION_ENABLED)
   val decommissionAllowMode = master.conf.get(MASTER_UI_DECOMMISSION_ALLOW_MODE)
 
   initialize()
@@ -60,11 +60,13 @@ class MasterWebUI(
         "/app/kill", "/", masterPage.handleAppKillRequest, httpMethods = Set("POST")))
       attachHandler(createRedirectHandler(
         "/driver/kill", "/", masterPage.handleDriverKillRequest, httpMethods = Set("POST")))
+    }
+    if (decommissionEnabled) {
       attachHandler(createServletHandler("/workers/kill", new HttpServlet {
         override def doPost(req: HttpServletRequest, resp: HttpServletResponse): Unit = {
           val hostnames: Seq[String] = Option(req.getParameterValues("host"))
             .getOrElse(Array[String]()).toSeq
-          if (decommissionDisabled || !isDecommissioningRequestAllowed(req)) {
+          if (!isDecommissioningRequestAllowed(req)) {
             resp.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED)
           } else {
             val removedWorkers = masterEndpointRef.askSync[Integer](
